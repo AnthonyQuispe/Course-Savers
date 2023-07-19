4ce843441b1f6bc95f4b05743dbb4b6145dfb912
Anthony Quispe <anthony.s.quispe@gmail.com>
2023-07-19 00:04:52 -0400

    Fixed tailwind config file + add variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,22 @@ import Image from "next/image";
 import logo from "../../public/Logo.svg";
 import Link from "next/link";
 
-import "./home.scss";
-
 export default function Home() {
   return (
     <Link href="/search">
-      {" "}
-      <main className="main">
-        <div className="main__background"></div>
-        <div className="main__container">
-          {" "}
+      <main className="relative flex justify-center items-center h-screen">
+        <div className="absolute inset-0 bg-purple-700 bg-opacity-75"></div>
+        <div className="absolute z-10">
           <Image
-            className="main__logo"
+            className="w-3/4 h-auto"
             src={logo}
             alt="logo image"
             width={200}
             height={200}
           />
-          <p className="main__text">Helping students spend wisely</p>
+          <p className="text-white text-5xl font-normal font-poppins">
+            Helping students spend wisely
+          </p>
         </div>
       </main>
     </Link>
