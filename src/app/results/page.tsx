5a3c375cb966543c58c98c9a7c2b7e7abfb7a379
Christian Ocampo <cocampo1005@yahoo.com>
2023-07-20 20:40:33 -0400

    Pulling other branch.

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image"
+import backButtonIcon from "../../../public/icons/back-button-icon.png";
+import filterIcon from "../../../public/icons/filter-icon.png";
 
 
 export default function Results() {
-    return <div className="">
-        
+    return <div className="p-4 pt-[44px]">
+        <div className="flex justify-between">
+            <Image src={backButtonIcon} alt="back button"/>
+            <Image src={filterIcon} alt="filter button"/>
+        </div>
+        <p className=" text-PrimaryPurp underline pl-2 font-bold">My Schedule</p>
+        <div></div>
     </div>
 }
