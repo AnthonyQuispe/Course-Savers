a1258517bef014898d6328dec2024dc86f75535c
Christian Ocampo <cocampo1005@yahoo.com>
2023-07-23 18:23:12 -0400

    Reverted the fixed and white bg attributes back to normal.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           playsInline
           width="100vw"
           height="100vh"
-          className="fixed w-screen h-screen object-cover bg-white"
+          className="w-screen h-screen object-cover"
         >
           <source src="videos/Course-Saver-Background.mp4" type="video/mp4" />
           Your browser does not support the video tag.
