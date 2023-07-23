8a71521c857f431ba63aa341c81b47e11a03a4ed
Christian Ocampo <cocampo1005@yahoo.com>
2023-07-23 16:32:57 -0400

    Added the week schedule day cards component.

---
+++ src/app/components/ResultsScheduleElement/ResultsScheduleElement.tsx
@@ -0,0 +1,22 @@
+import { Carousel } from "react-responsive-carousel"
+
+
+const DayCard = ({ day }) => (
+    <div className="flex flex-col mx-1 items-center justify-center min-w-[200px] h-44 bg-white rounded-2xl">
+        <p className="text-4xl pb-3 text-DarkPurp">{day}</p>
+        <p className="text-BlackText">---</p>
+        <p className="text-BlackText">---</p>
+    </div>
+)
+
+export default function ResultsScheduleElement() {
+    const days = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+
+    return (
+        <div className="flex overflow-x-auto drop-shadow-xl px-5">
+            {days.map((day, index) => (
+                <DayCard key={index} day={day} />
+            ))}
+        </div>
+    )
+}
--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image"
 import backButtonIcon from "../../../public/icons/back-button-icon.png";
 import filterIcon from "../../../public/icons/filter-icon.png";
+import ResultsScheduleElement from "../components/ResultsScheduleElement/ResultsScheduleElement";
 
 
 export default function Results() {
-    return <div className="p-4 pt-[44px]">
-        <div className="flex justify-between">
+    return <div className="pt-[44px]">
+        <div className="flex justify-between px-3">
             <Image src={backButtonIcon} alt="back button"/>
             <Image src={filterIcon} alt="filter button"/>
         </div>
-        <p className=" text-PrimaryPurp underline pl-2 font-bold">My Schedule</p>
-        <div></div>
+        <p className=" text-PrimaryPurp underline pl-6 pb-3 font-bold">My Schedule</p>
+        <div>
+            <ResultsScheduleElement />
+        </div>
     </div>
 }
