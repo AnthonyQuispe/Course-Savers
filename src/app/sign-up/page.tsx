0632e4f7268ec7f46074adda01a51206128fed16
Jose Aparicio <jfacademusic@gmail.com>
2023-07-24 17:01:27 -0400

    sign up page styling done

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -32,6 +32,7 @@ import { registerSchema } from "@/validators/auth"
 import { z } from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import Link from "next/link"
 
 //puts a type to the input object from registerSchema
 type Input = z.infer<typeof registerSchema>;
@@ -60,23 +61,23 @@ export default function SignUp() {
     }
 
     return (
-        <div className="w-[100dvw] h-[100dvh] bg-[white] text-black flex justify-center">
-            <div className="w-full h-full bg-center flex justify-center items-center">
-                <Card className="border-none flex flex-col w-full px-6  ">
+        <div className="w-[100dvw] h-[100dvh] max-h-[100dvh] bg-[white] text-black flex justify-center">
+                <Card className=" absolute top-[15%] shadow-gray-300 rounded shadow-lg border-t-0 flex flex-col border-none w-[85%] max-h-[80dvh] overflow-scroll px-4 py-8">
                     <CardHeader className="p-0">
-                        <CardTitle className="text-[#390A75] mb-4">Sign Up</CardTitle>
+                        <CardTitle className="text-[#390A75] mb-8">Sign Up</CardTitle>
                     </CardHeader>
-                    <CardContent className="p-0">
-                        <Form {...form}>
+                    <CardContent className="p-0 pl-2 ">
+                        <Form {...form} >
                             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
                                 <FormField
                                     control={form.control}
                                     name="firstName"
                                     render={({ field }) => (
-                                        <FormItem>
+                                        <FormItem >
                                             <FormLabel>First Name</FormLabel>
                                             <FormControl>
-                                                <Input placeholder="First Name" {...field} />
+                                                <Input placeholder="First Name" className="bg-[#F3F3F3] rounded border-0"
+                                                    {...field} />
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
@@ -89,7 +90,7 @@ export default function SignUp() {
                                         <FormItem>
                                             <FormLabel>Last Name</FormLabel>
                                             <FormControl>
-                                                <Input placeholder="Last Name" {...field} />
+                                                <Input placeholder="Last Name" {...field} className="bg-[#F3F3F3] rounded border-0"/>
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
@@ -103,11 +104,11 @@ export default function SignUp() {
                                             <FormLabel>School</FormLabel>
                                             <Select onValueChange={field.onChange} defaultValue={field.value}>
                                                 <FormControl>
-                                                    <SelectTrigger>
+                                                    <SelectTrigger className="bg-[#F3F3F3] rounded border-0">
                                                         <SelectValue placeholder="Select your School" />
                                                     </SelectTrigger>
                                                 </FormControl>
-                                                <SelectContent className="bg-[#390A75] text-white">
+                                                <SelectContent className="bg-[#F3F3F3] rounded border-0">
                                                     <SelectItem value="Florida International Univeristy">Florida International Univeristy</SelectItem>
                                                     <SelectItem value="Univeristy of Miami">Univeristy of Miami</SelectItem>
                                                     <SelectItem value="Miami Dade College">Miami Dade College</SelectItem>
@@ -125,7 +126,7 @@ export default function SignUp() {
                                         <FormItem>
                                             <FormLabel>Student ID</FormLabel>
                                             <FormControl>
-                                                <Input placeholder="Student I.D." {...field} />
+                                                <Input placeholder="Student I.D." className="bg-[#F3F3F3] rounded border-0" {...field} />
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
@@ -138,7 +139,7 @@ export default function SignUp() {
                                         <FormItem>
                                             <FormLabel>School Email</FormLabel>
                                             <FormControl>
-                                                <Input placeholder="Email" {...field} />
+                                                <Input placeholder="Email" {...field} className="bg-[#F3F3F3] rounded border-0"/>
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
@@ -151,7 +152,7 @@ export default function SignUp() {
                                         <FormItem>
                                             <FormLabel>Password</FormLabel>
                                             <FormControl>
-                                                <Input placeholder="Password" {...field} type="password" />
+                                                <Input placeholder="Password" {...field} type="password" className="bg-[#F3F3F3] rounded border-0"/>
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
@@ -164,18 +165,17 @@ export default function SignUp() {
                                         <FormItem>
                                             <FormLabel>Confirm Password</FormLabel>
                                             <FormControl>
-                                                <Input placeholder="Confirm Password" {...field} type="password" />
+                                                <Input placeholder="Confirm Password" {...field} type="password" className="bg-[#F3F3F3] rounded border-0"/>
                                             </FormControl>
                                             <FormMessage />
                                         </FormItem>
                                     )}
                                 />
-                                <Button type="submit">Submit</Button>
+                                <Button type="submit" className="w-full bg-[#470D92] text-[#FCFCFC] rounded-[20px]">Submit</Button>
                             </form>
                         </Form>
                     </CardContent>
                 </Card>
-            </div>
         </div>
     )
 }
