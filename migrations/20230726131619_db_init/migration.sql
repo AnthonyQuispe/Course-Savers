74de249cb67bcd6c7300961a9bfeece047085cf5
Jose Aparicio <jfacademusic@gmail.com>
2023-07-27 15:20:01 -0400

    deleted old migrations

--- migrations/20230726131619_db_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Users_schoolsId_fkey` ON `users`;
-
--- AddForeignKey
-ALTER TABLE `Users` ADD CONSTRAINT `Users_schoolsId_fkey` FOREIGN KEY (`schoolsId`) REFERENCES `Schools`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
