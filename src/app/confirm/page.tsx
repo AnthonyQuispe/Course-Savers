ccc6f8e38304eef0d6307670e81a849d3c489178
Christian Ocampo <cocampo1005@yahoo.com>
2023-07-28 07:04:27 -0400

    Finished styling confirm page with functioning navigation buttons. Just need to connect check-out page with backend.

--- src/app/confirm/page.tsx
+++ src/app/confirm/page.tsx
@@ -3,8 +3,11 @@
 import Image from "next/image";
 import backButtonIcon from "../../../public/icons/back-button-icon.png";
 import financesIcon from "../../../public/icons/finances-icon.png";
+import { useRouter } from "next/navigation";
 
 export default function Confirm() {
+
+    const router = useRouter();
 
     return (
         <div className="pt-[44px]">
@@ -12,10 +15,12 @@ export default function Confirm() {
                 <Image src={backButtonIcon} alt="back button" onClick={() => router.back()} />
                 <Image className="w-12 h-12" src={financesIcon} alt="finances button" />
             </div>
-            <section className="p-6 pt-2">
-                
-            </section>
-            <section className="px-6">
+            <section className="p-6 pt-2 flex flex-col justify-between h-full">
+                <h1 className=" text-xl text-PrimaryPurp font-extrabold">CONFIRMATION</h1>
+                <div className="py-[15rem] px-6">
+                    <h2 className=" text-PrimaryPurp text-lg font-extrabold pb-4">You're Done!</h2>
+                    <p className=" text-lg">You've successfully finalized your classes for the term.</p>
+                </div>
                 <button className="bg-PrimaryPurp rounded-full w-full p-3 text-white text-xl font-extrabold">Check Your Finances</button>
             </section>
         </div>
