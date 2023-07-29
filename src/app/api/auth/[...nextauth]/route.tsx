40450de54d66fd53872c437249a668a0169f61e2
Anthony Quispe <anthony.s.quispe@gmail.com>
2023-07-29 18:15:29 -0400

    fixed eslint erros

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,58 +1,57 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextApiHandler } from "next";
+import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 export const authOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "email", type: "text" },
-                password: { label: "password", type: "password" }
-            },
-            async authorize(credentials) {
-                //check to see if email and password is valid
-                //if no email or password
-                if (!credentials?.email : string || !credentials.password) {
-                    return null;
-                }
-                //check to see if user exists
-                const user = await prisma.users.findUnique({
-                    where: { email: credentials.email }
-                })
-                //if user doesn't exist
-                if (!user) {
-                    return null;
-                }
-                //check if password matches username
-                const passwordsMatch = await bcrypt.compare(credentials.password, user.password);
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
 
-                //if password doesn't match
-                if (!passwordsMatch) {
-                    console.log(credentials.password)
-                    console.log(user.password)
-                    console.log('passwords do not match')
-                    return null;
-                }
+        const user = await prisma.users.findUnique({
+          where: { email: credentials.email },
+        });
 
-                //return user object
-                return user;
-            }
-        })
-    ],
-    session: {
-        strategy: "jwt",
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    debug: process.env.NODE_ENV === "development",
+        if (!user) {
+          return null;
+        }
+
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!passwordsMatch) {
+          console.log(credentials.password);
+          console.log(user.password);
+          console.log("passwords do not match");
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
+  //   session: {
+  //     jwt: true, // Use JWT as the session strategy
+  //   },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === "development",
 };
 
-const handler = NextAuth(authOptions)
+const handler: NextApiHandler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export default handler;
