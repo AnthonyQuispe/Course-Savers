ae43e257d53aba46a107012b571ad2aa82dbf8be
Jose Aparicio <jfacademusic@gmail.com>
2023-07-29 17:11:56 -0400

    minor changes

--- src/app/check-out/page.tsx
+++ src/app/check-out/page.tsx
@@ -5,11 +5,29 @@ import backButtonIcon from "../../../public/icons/back-button-icon.png";
 import financesIcon from "../../../public/icons/finances-icon.png";
 import infoIcon from "../../../public/icons/info-icon.png";
 import trashIcon from "../../../public/icons/trash-icon.png";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+async function getClassData(classId: number) {
+    const request = await fetch(`/api/getClassInfo?classId=${classId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    return request.json()
+}
 
 export default function CheckOut() {
 
     const router = useRouter();
+    const searchParams = useSearchParams()
+
+    const getClassId = searchParams.get('classId') ?? ''
+
+    useEffect(() => {
+        getClassData(Number(getClassId))
+    }, [])
 
     return (
         <div className="pt-[44px]">
@@ -19,27 +37,27 @@ export default function CheckOut() {
             </div>
             <section className="p-6 pt-2">
                 <p className=" pb-4 text-DarkPurp text-xl font-extrabold">CART</p>
-                    <article className="rounded-3xl shadow-lg">
-                        <div className="bg-GrayPurp flex items-center justify-between p-4 rounded-t-3xl">
-                            <h2 className="text-xl font-extrabold">NURS 101</h2>
-                            <Image src={infoIcon} alt="More info button" />
-                        </div> 
-                        <div className="flex items-center p-4 leading-7">
-                            <Image src={trashIcon} alt="delete button" />
-                            <div className="mx-4 font-medium">
-                                <p>Dates:</p>
-                                <p>When:</p>
-                                <p>Instructor:</p>
-                                <p>Cost:</p>
-                            </div>
-                            <div className=" font-light">
-                                <p>06/19-07/18</p>
-                                <p>M, W @ 11a-12p</p>
-                                <p>Dr. Thornberry</p>
-                                <p>$400</p>
-                            </div>
+                <article className="rounded-3xl shadow-lg">
+                    <div className="bg-GrayPurp flex items-center justify-between p-4 rounded-t-3xl">
+                        <h2 className="text-xl font-extrabold">NURS 101</h2>
+                        <Image src={infoIcon} alt="More info button" />
+                    </div>
+                    <div className="flex items-center p-4 leading-7">
+                        <Image src={trashIcon} alt="delete button" />
+                        <div className="mx-4 font-medium">
+                            <p>Dates:</p>
+                            <p>When:</p>
+                            <p>Instructor:</p>
+                            <p>Cost:</p>
                         </div>
-                    </article>
+                        <div className=" font-light">
+                            <p>06/19-07/18</p>
+                            <p>M, W @ 11a-12p</p>
+                            <p>Dr. Thornberry</p>
+                            <p>$400</p>
+                        </div>
+                    </div>
+                </article>
             </section>
             <section className="px-6">
                 <h2 className=" text-xl font-medium pt-3 pb-2">Course Costs</h2>
